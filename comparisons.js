15efe7c8fe1e8ba1a9d6a669b446a5f87471c0c0
Mike Ward <mike@neptuneflood.com>
2023-12-18 11:33:22 -0500

    Adding Comparisons File

---
+++ comparisons.js
@@ -0,0 +1,19 @@
+console.log("Does 5 = 4? " + (5 == 4))
+
+console.log("Does 5 = 5? " + (5 == 5))
+
+console.log("Does 4 not equal 5? " + (4 != 5))
+
+console.log("Is 5 greater than 4? " + (5 > 4))
+console.log("Is 5 less than 4? " + (5 < 4))
+console.log("Is 4 less than or equal to 5? " + (4 <=5))
+console.log("Is 5 greater than or equal to 5? " + (5 >= 5))
+
+let x = 3
+let y = 2
+
+console.log("Is x greater than y? " + (x > y))
+console.log("Is x equal to y? " + (x ==y))
+
+//Don't do this; this assigns variable value to x, rather than comparing x to y
+console.log(x = y)
