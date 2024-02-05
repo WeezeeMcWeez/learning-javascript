a098879439d5083ed77219ffde482329da01c3ba
Mike Ward <mike@neptuneflood.com>
2024-02-05 10:39:18 -0500

    classes code

---
+++ classes.js
@@ -0,0 +1,16 @@
+class Cat {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+}
+
+const myFirstCat = new Cat('Nippers', 11)
+
+console.log(myFirstCat)
+console.log(myFirstCat.name)
+
+const mySecondCat = new Cat ('Little Debbie', 'deceased')
+
+console.log(mySecondCat.age)
+console.log(mySecondCat)
